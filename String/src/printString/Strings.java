0e89fd5fd76fa4358b7646d97b4d20d91bad60a6
Abhi-Kushwah <abhishekkushwah75094@gmail.com>
2025-02-14 00:39:12 +0300

    using for loop

---
+++ String/src/printString/Strings.java
@@ -0,0 +1,9 @@
+package printString;
+
+public class Strings {
+	public static void main(String[] args) {
+		int a='A';
+		System.out.println("value of A : "+a);
+	}
+
+}
---
+++ String/src/stringnum/String.java
@@ -0,0 +1,8 @@
+package stringnum;
+
+public class String {
+	public static void main(String[] args) {
+	int a=45;
+	System.out.println("string numeric value of a :"+a);
+   }	
+}
